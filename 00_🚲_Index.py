8d10cf344ef8fb0058d9facd15060a9f34e116c3
payaneco <6695369+payaneco@users.noreply.github.com>
2022-09-11 17:04:24 +0900

    changed

--- 00_🚲_Index.py
+++ 00_🚲_Index.py
@@ -1,3 +1,47 @@
 import streamlit as st
+from bokeh.models.widgets import Button
+from bokeh.models import CustomJS
+from streamlit_bokeh_events import streamlit_bokeh_events
+import streamlit.components.v1 as components
 
-st.write("hello")
+html_format = """
+    <script src="https://aframe.io/releases/1.0.4/aframe.min.js"></script>
+    <script src="https://unpkg.com/aframe-look-at-component@0.8.0/dist/aframe-look-at-component.min.js"></script>
+    <script src="https://raw.githack.com/AR-js-org/AR.js/master/aframe/build/aframe-ar-nft.js"></script>
+    <a-scene vr-mode-ui="enabled: false" embedded arjs="sourceType: webcam; debugUIEnabled: false;">
+      <a-text value="PC1" look-at="[gps-camera]" scale="{0} {0} {0}" gps-entity-place="latitude: {1}; longitude: {2};"></a-text>
+      <a-camera gps-camera rotation-reader> </a-camera>
+    </a-scene>
+    <a-scene vr-mode-ui="enabled: false" embedded arjs="sourceType: webcam; debugUIEnabled: false;">
+      <a-text value="PC1" look-at="[gps-camera]" scale="{0} {0} {0}" gps-entity-place="latitude: {3}; longitude: {4};"></a-text>
+      <a-camera gps-camera rotation-reader> </a-camera>
+    </a-scene>
+"""
+
+st.write("現在地を取得すると周辺にARマークを生成します")
+
+loc_button = Button(label="現在地を取得")
+loc_button.js_on_event("button_click", CustomJS(code="""
+    navigator.geolocation.getCurrentPosition(
+        (loc) => {
+            document.dispatchEvent(new CustomEvent("GET_LOCATION", {detail: {lat: loc.coords.latitude, lng: loc.coords.longitude}}))
+        }
+    )
+    """))
+result = streamlit_bokeh_events(
+    loc_button,
+    events="GET_LOCATION",
+    key="get_location",
+    refresh_on_update=False,
+    override_height=75,
+    debounce_time=0)
+
+if result:
+    lat = result["GET_LOCATION"]["lat"]
+    lng = result["GET_LOCATION"]["lng"]
+    lat1 = lat + 0.000001
+    lng1 = lng
+    lat2 = lat - 0.000001
+    lng2 = lng
+    html = html_format.format(60, lat1, lng1, lat2, lng2)
+    components.html(html, height=480)
