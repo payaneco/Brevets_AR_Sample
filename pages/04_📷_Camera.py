ce53b729396a9cd647e8ef770b8c82bb758d15b9
payaneco <6695369+payaneco@users.noreply.github.com>
2022-09-11 15:28:42 +0900

    new file:   "00_\360\237\232\262_Index.py" 	new file:   "pages/04_\360\237\223\267_Camera.py"

---
+++ pages/04_📷_Camera.py
@@ -0,0 +1,33 @@
+import streamlit as st
+import streamlit.components.v1 as components
+
+components.html("""
+    <script src="https://aframe.io/releases/1.0.4/aframe.min.js"></script>
+    <script src="https://unpkg.com/aframe-look-at-component@0.8.0/dist/aframe-look-at-component.min.js"></script>
+    <script src="https://raw.githack.com/AR-js-org/AR.js/master/aframe/build/aframe-ar-nft.js"></script>
+    <a-scene
+      vr-mode-ui="enabled: false"
+      embedded
+      arjs="sourceType: webcam; debugUIEnabled: false;"
+    >
+      <a-text
+        id="ar-pc1"
+        value="BRM999 PC1"
+        look-at="[gps-camera]"
+        scale="60 60 60"
+        gps-entity-place="latitude: {my-lat}; longitude: {my-long};"
+      ></a-text>
+      <a-camera gps-camera rotation-reader> </a-camera>
+    </a-scene>
+    <script>
+        function getLocation() {
+            navigator.geolocation.getCurrentPosition(setLatLong);
+        }
+        function setLatLong(position) {
+            let ar-tag = document.getElementById("ar-pc1");
+            let p = "latitude: " + position.coords.latitude + "; longitude: + " + position.coords.longitude +";";
+            ar-tag.setAttribute("gps-entity-place", p);
+        }
+        window.onload = getLocation;
+    </script>
+""", height=480)
